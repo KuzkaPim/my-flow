c981c80cf0831a241c0b8ac85bf569dc0ba38085
Kuzma <kuzkapimenov@gmail.com>
2025-07-16 17:42:14 +0500

    Added mixin btn and use it

--- src/Components/Cart/Cart.tsx
+++ src/Components/Cart/Cart.tsx
@@ -11,7 +11,7 @@ const Cart = () => {
 	return (
 		<div className={styles.cart}>
 			<h2 className={styles.title}>Cart</h2>
-			{cart.length ? (
+			{cart.length !== 0 ? (
 				<ul className={styles.cartList}>
 					{cart.map((item) => (
 						<CartItem key={item.dish.name} cartDish={item} />
