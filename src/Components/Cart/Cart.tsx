439708440d2b3842a2fdf9e89d09dae1c66d1226
Kuzma <kuzkapimenov@gmail.com>
2025-07-19 20:31:30 +0500

    Add clear cart btn

--- src/Components/Cart/Cart.tsx
+++ src/Components/Cart/Cart.tsx
@@ -1,23 +1,59 @@
 'use client';
 
 import { useAppSelector } from '@/hooks/useAppSelector';
+import Image from 'next/image';
 import styles from './Cart.module.sass';
 import CartItem from '../CartItem/CartItem';
 import CartCheckout from '../CartCheckout/CartCheckout';
+import Modal from '../UI/Modal/Modal';
+import basketImg from './basket.svg';
+import { useAppDispatch } from '@/hooks/useAppDispatch';
+import { toggleModalCart, clearCart } from '@/store/cart/cartSlice';
 
 const Cart = () => {
-	const { cart } = useAppSelector((state) => state.cart);
+	const { cart, isShowModalCart } = useAppSelector((state) => state.cart);
+	const dispatch = useAppDispatch();
+
+	const totalPrice = cart.reduce((sum, item) => {
+		return sum + item.dish.price * item.count;
+	}, 0);
 
 	return (
 		<div className={styles.cart}>
 			<h2 className={styles.title}>Cart</h2>
 			{cart.length === 0 && <p className={styles.empty}>Empty</p>}
-			<ul className={styles.cartList}>
-				{cart.map((item) => (
-					<CartItem key={item.dish.name} cartDish={item} />
-				))}
-			</ul>
-			{cart.length > 0 && <CartCheckout />}
+			{cart.length > 0 && (
+				<>
+					<ul className={styles.cartList}>
+						{cart.map((item) => (
+							<CartItem key={item.dish.name} cartDish={item} />
+						))}
+					</ul>
+					<span className={styles.price}>= {totalPrice} ₸</span>
+					<CartCheckout totalPrice={totalPrice} />
+					<div className={styles.controls}>
+						<button
+							onClick={() => dispatch(clearCart())}
+							className={styles.basketBtn}
+						>
+							<Image
+								className={styles.basketImg}
+								src={basketImg}
+								width={26}
+								height={26}
+								alt='Basket'
+							/>
+						</button>
+						<button
+							onClick={() => dispatch(toggleModalCart())}
+							className={styles.orderBtn}
+						>
+							Place order
+						</button>
+					</div>
+					{isShowModalCart && <Modal />}
+				</>
+			)}
 		</div>
 	);
 };
--- src/Components/CartCheckout/CartCheckout.tsx
+++ src/Components/CartCheckout/CartCheckout.tsx
@@ -1,20 +1,14 @@
-import { useAppSelector } from '@/hooks/useAppSelector';
 import styles from './CartCheckout.module.sass';
-import Modal from '../UI/Modal/Modal';
-import { toggleModalCart } from '@/store/cart/cartSlice';
-import { useAppDispatch } from '@/hooks/useAppDispatch';
 
-const CartCheckout = () => {
-	const { cart, isShowModalCart } = useAppSelector((state) => state.cart);
-	const totalCartPrice = cart.reduce((sum, item) => {
-		return sum + item.dish.price * item.count;
-	}, 0);
-	const totalPrice = totalCartPrice + 1000;
-	const dispatch = useAppDispatch();
+type TProps = {
+	totalPrice: number;
+};
+
+const CartCheckout = ({ totalPrice }: TProps) => {
+	totalPrice += 1000;
 
 	return (
 		<div className={styles.cartCheckout}>
-			<span className={styles.price}>= {totalCartPrice} ₸</span>
 			<div className={styles.priceInfo}>
 				<span className={styles.priceInfoItem}>
 					<span className={styles.priceInfoTitle}>Delivery:</span>1000 ₸
@@ -24,13 +18,6 @@ const CartCheckout = () => {
 					{totalPrice} ₸
 				</span>
 			</div>
-			<button
-				onClick={() => dispatch(toggleModalCart())}
-				className={styles.orderBtn}
-			>
-				Place order
-			</button>
-			{isShowModalCart && <Modal />}
 		</div>
 	);
 };
